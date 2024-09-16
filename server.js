95cc76dc77679bfe16a2d55775d22998208a0b25
ramnivaa <nivas0510@gmail.com>
2024-09-16 22:28:56 +0530

    list filter mock json

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use('/', express.static('dist/product-catalog-app/'));
+app.use('products', express.static('dist/product-catalog-app/'));
+app.use('/products', express.static('dist/product-catalog-app/'));
+app.use('about', express.static('dist/product-catalog-app/'));
+app.use('/about', express.static('dist/product-catalog-app/'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
